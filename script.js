9c541c2d6c86280fbe9e6606be6a1ff0b2f03a4b
erenbugrayolcu <63672367+erenbugrayolcu@users.noreply.github.com>
2021-08-16 20:15:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function generate(){
         "- Alysha Speer" : "You never really know what's coming. A small wave, or maybe a big one. All you can really do is hope that when it comes, you can surf over it, instead of drown in its monstrosity.",
         "- C.S. Lewis" : "I was not born to be free---I was born to adore and obey.",
         "- John Galsworthy" : "Life calls the tune, we dance.",
-        "- Jenny Valentine" : "Even when you’d lost everything you thought there was to lose, somebody came along and gave you something for free."
+        "- Jenny Valentine" : "Even when you’d lost everything you thought there was to lose, somebody came along and gave you something for free.",
+        "- Adam Jensen : "I never asked for this."
     }
 
     var authors = Object.keys(quotes);
